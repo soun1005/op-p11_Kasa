d0eb47f82caf53586298d28eb536197e479dd557
Soeun Lee <thdms1005@gmail.com>
2023-02-07 16:03:11 +0100

    footer component added

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,30 @@ import About from './pages/About/index';
 import LocationDetails from './pages/LocationDetails';
 // import Accommodation from './pages/Accommodation';
 import Error404 from './pages/Error404/index';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
+    <>
       <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route
-            exact
-            path="details/:locationId"
-            element={<LocationDetails />}
-          />
-          <Route exact path="about" element={<About />} />
-          <Route exact path="*" element={<Error404 />} />
-        </Routes>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route
+              exact
+              path="details/:locationId"
+              element={<LocationDetails />}
+            />
+            <Route exact path="about" element={<About />} />
+            <Route exact path="*" element={<Error404 />} />
+          </Routes>
+        </BrowserRouter>
       </main>
-    </BrowserRouter>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 }
 
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import style from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <div className={style.container}>
+      <Logo className={style.logo} />
+      <span className={style.rights}>© 2020 Kasa. All rights reserved</span>
+    </div>
+  );
+}
